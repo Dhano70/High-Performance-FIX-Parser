cd27eece08b5e23e937e818304d106acafe999db
Dhano70 <denizhan.ozdemirnl@gmail.com>
2026-06-16 22:00:34 +0200

    updated style and added fileSize check

--- main.cpp
+++ main.cpp
@@ -12,30 +12,33 @@ int main() {
         return 1;
     }
 
-    //retrieve file size
     struct stat fileStat;
     if (fstat(fd, &fileStat) == -1) {
         std::cout << "Error getting file stats" << std::endl;
         close(fd);
         return 1;
     }
-    size_t fileSize = fileStat.st_size;
 
-    //map file into process memory
+    size_t fileSize = fileStat.st_size;
+    if (fileSize < 4) {
+        std::cout << "File too small\n";
+        close(fd);
+        return 0;
+    }
+
     char* data = (char*)mmap(nullptr, fileSize, PROT_READ, MAP_PRIVATE, fd, 0);
     if (data == MAP_FAILED) {
         std::cerr << "Mapping failed!" << std::endl;
+        close(fd);
         return 1;
     }
 
-   
     std::cout << "Successfully mapped " << fileSize << " bytes." << std::endl;
 
     int buyCount = 0;
-
-    // stop 4 bytes early to look ahead for "54=1" and not read out of bound
     for (size_t i = 0; i < fileSize - 4; ++i) {
-        if (data[i] == '5' && data[i+1] == '4' && data[i+2] == '=' && data[i+3] == '1') {
+        if (data[i] == '5' && data[i+1] == '4' && 
+            data[i+2] == '=' && data[i+3] == '1') {
             buyCount++;
             i += 3; // Skip the characters that matched
         }
