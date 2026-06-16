35235bec59d830806ab0fdda0da053d0bb70ce3a
Dhano70 <denizhan.ozdemirnl@gmail.com>
2026-06-16 22:02:25 +0200

    changed cout to cerr stream

--- main.cpp
+++ main.cpp
@@ -14,21 +14,21 @@ int main() {
 
     struct stat fileStat;
     if (fstat(fd, &fileStat) == -1) {
-        std::cout << "Error getting file stats" << std::endl;
+        std::cerr << "Error: getting file stats" << std::endl;
         close(fd);
         return 1;
     }
 
     size_t fileSize = fileStat.st_size;
     if (fileSize < 4) {
-        std::cout << "File too small\n";
+        std::cerr << "Error: file too small\n";
         close(fd);
         return 0;
     }
 
     char* data = (char*)mmap(nullptr, fileSize, PROT_READ, MAP_PRIVATE, fd, 0);
     if (data == MAP_FAILED) {
-        std::cerr << "Mapping failed!" << std::endl;
+        std::cerr << "Error: mapping failed!" << std::endl;
         close(fd);
         return 1;
     }
