af0ab879f9e8359e5cc8e8bd22e15dad00e20620
Dhano70 <denizhan.ozdemirnl@gmail.com>
2026-07-01 12:06:10 +0200

    adjusted data set to be have mor messages and various prices

--- generate_data.py
+++ generate_data.py
@@ -11,12 +11,13 @@ Output: test_data.txt
 import random
 
 with open("test_data.txt", "w") as f:
-    for i in range(100000):
-        side = random.choice(['1', '2'])  # 1 for buy, 2 for sell
-        price = random.randint(850, 950)  
-        qty = random.randint(1, 100)      
+    for i in range(1000000): 
+        side = random.choice(['1', '2'])  
+        price = round(random.uniform(850.00, 950.00), 2) 
+        qty = random.randint(1, 1000)      
 
+        #FIX message layout
         message = f"8=FIX.4.2|35=D|49=ME|56=EXCHANGE|55=ASML|54={side}|38={qty}|44={price}|"
         f.write(message + "\n")
 
-print("File successfully created with 100,000 lines")
+print("File successfully created with 1,000,000 lines")
